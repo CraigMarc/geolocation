c8f355e218ca23cd9f143ee74e11416ef8283fed
craigmarc <cmarcinkiewicz2000@yahoo.com>
2024-05-10 11:21:48 -0400

    set up template

---
+++ App.js
@@ -0,0 +1,26 @@
+//import React from 'react';
+import { useState, useEffect } from 'react'
+
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+    <Text> Loading
+    </Text>
+    </View>
+
+
+  );
+};
+
+export default App;
